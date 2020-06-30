be6af84edd0937334a6b19e98d6e35c414e05637
vydh2 <vydh2@vng.com.vn>
2020-07-01 01:43:24 +0700

    Updates

---
+++ precache-manifest.2f4a28136a9272b5e0d6ca15f1f40595.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2104da82619b46afeab5a8624fe8eb9e",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "651d03db1a8c0c1f4f9c",
+    "url": "/robofriend/static/css/main.b0a3c61b.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriend/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriend/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651d03db1a8c0c1f4f9c",
+    "url": "/robofriend/static/js/main.63e393c3.chunk.js"
+  },
+  {
+    "revision": "73b0268b71309baed9b7",
+    "url": "/robofriend/static/js/runtime-main.2d85b6a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
